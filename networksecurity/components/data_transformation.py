12a9ae18c7cc7d3be3b5d89f255d30e9a7ca6fb3
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-03-09 22:22:56 +0530

    transformation knn

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,15 @@
+import sys,os,numpy as np,pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.constant.training_pipeline import TARGET_COLUMN,DATA_TRANSFORMATION_IMPUTER_PARAMS
+from networksecurity.entity.artifact_entity import(
+    DataTransformationArtifact,
+    DataValidationArtifact
+)
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+
+from networksecurity.logging.logger import logging
+
+from networksecurity.utils.main_utils.utils import save_numpy_array_data,save_object
